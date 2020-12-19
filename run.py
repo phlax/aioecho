7bac219ad3e0369b0947e45b67f41d51066b0f4f
Ryan Northey <ryan@synca.io>
2020-12-19 18:41:34 +0000

    run.py

--- run.py
+++ run.py
@@ -1,11 +1,14 @@
 
 import asyncio
+import logging
 import os
 import socket
 import ssl
 
 from aiohttp import web
 
+
+logger = logging.getLogger(__name__)
 
 runners = []
 
@@ -29,42 +32,39 @@ async def start_site(app, address='0.0.0.0', port=9100, tls=False):
     await site.start()
 
 
-async def _response(protocol, request):
+async def handler(request):
     content = await request.text()
-    return web.json_response(
-        dict(protocol=request.scheme,
-             http=(f'{request.version.major}/'
-                   f'{request.version.minor}'),
-             method=request.method,
-             remote=request.remote,
-             path=request.path,
-             keep_alive=request.keep_alive,
-             headers=dict(request.headers),
-             content=content,
-             cookies=dict(request.cookies),
-             query=dict(request.query),
-             os=dict(
-                 hostname=socket.gethostname())))
-
-
-async def http_handler(request):
-    return await _response('http', request)
-
-
-async def https_handler(request):
-    return await _response('https', request)
+    response = dict(
+        protocol=request.scheme,
+        http=(f'{request.version.major}/'
+              f'{request.version.minor}'),
+        method=request.method,
+        remote=request.remote,
+        path=request.path,
+        keep_alive=request.keep_alive,
+        headers=dict(request.headers),
+        content=content,
+        cookies=dict(request.cookies),
+        query=dict(request.query),
+        os=dict(
+            hostname=socket.gethostname()))
+    logger.info(response)
+    return web.json_response(response)
 
 
 if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO)
     loop = asyncio.get_event_loop()
     app = web.Application()
-    app.router.add_route('*', '/', http_handler)
+    app.router.add_route('*', '/{tail:.*}', handler)
     http = os.environ.get('HTTP_PORT')
     https = os.environ.get('HTTPS_PORT')
     if http:
+        logging.info(f'Starting HTTP server on port {http}')
         loop.create_task(
             start_site(app, port=http))
     if https:
+        logging.info(f'Starting HTTPS server on port {https}')
         loop.create_task(
             start_site(app, port=https, tls=True))
     try:
