1a32dbaac7afd0add43883ace437fe32f299228f
Ryan Northey <ryan@synca.io>
2020-12-19 18:07:44 +0000

    run.py

--- run.py
+++ run.py
@@ -1,11 +1,14 @@
 
 import asyncio
+import os
 import socket
 import ssl
 
 from aiohttp import web
 
+
 runners = []
+
 
 async def start_site(app, address='0.0.0.0', port=9100, tls=False):
     runner = web.AppRunner(app)
@@ -13,14 +16,17 @@ async def start_site(app, address='0.0.0.0', port=9100, tls=False):
     await runner.setup()
 
     if tls:
-        ssl_context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
-        ssl_context.load_cert_chain('/certs/servercert.pem', '/certs/serverkey.pem')
-        site = web.TCPSite(runner, address, port, ssl_context=ssl_context)
+        ssl_context = ssl.create_default_context(
+            ssl.Purpose.CLIENT_AUTH)
+        ssl_context.load_cert_chain(
+            '/certs/servercert.pem',
+            '/certs/serverkey.pem')
+        site = web.TCPSite(
+            runner, address, port, ssl_context=ssl_context)
     else:
-        site = web.TCPSite(runner, address, port)
+        site = web.TCPSite(
+            runner, address, port)
     await site.start()
-
-loop = asyncio.get_event_loop()
 
 
 async def _response(protocol, request):
@@ -49,18 +55,22 @@ async def https_handler(request):
     return await _response('https', request)
 
 
-app1 = web.Application()
-app1.router.add_route('*', '/', http_handler)
-app2 = web.Application()
-app2.router.add_route('*', '/', https_handler)
-
-loop.create_task(start_site(app1))
-loop.create_task(start_site(app2, port=9101, tls=True))
-
-try:
-    loop.run_forever()
-except:
-    pass
-finally:
-    for runner in runners:
-        loop.run_until_complete(runner.cleanup())
+if __name__ == '__main__':
+    loop = asyncio.get_event_loop()
+    app = web.Application()
+    app.router.add_route('*', '/', http_handler)
+    http = os.environ.get('HTTP_PORT')
+    https = os.environ.get('HTTPS_PORT')
+    if http:
+        loop.create_task(
+            start_site(app, port=http))
+    if https:
+        loop.create_task(
+            start_site(app, port=https, tls=True))
+    try:
+        loop.run_forever()
+    except:
+        pass
+    finally:
+        for runner in runners:
+            loop.run_until_complete(runner.cleanup())
